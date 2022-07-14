028cc357d7b4aee94bc9c74ee641da7b3894b71b
rayyanabahadirova <reyyanmebahadirovaa@gmail.com>
2022-07-14 08:40:43 +0300

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './App.css'
+
+
+
+const App = () => {
+  return (
+    <div  className='main'>
+          <nav>
+        <img src="images/logo.jpg" alt="" width="100px"/>
+        <div className="menu">
+            <ul className="nav-links">
+                <li className="li1">Bosh saxifa</li>
+                <li className="li1"> Jamoa</li>
+                <li className="li1">Kontaktlar</li>
+                <li className="li1">Blog</li>
+                <li className='li1'>Xizmatlar</li>
+            <button className="button-29" role="button">+998 90 921 37 11</button>
+            </ul>
+            
+        </div>
+     
+    </nav>
+      <div className="container">
+      <div className="text">
+      <h1 className='one'>Biznesingizni keyingi bosqichga olib chiqing</h1> <br />
+      <p>Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget sollicitudin massa pellentesque in. Vivamus mattis eros at sem pulvinar</p> <br />
+
+      <button className=" ona">Xizmatar bilan tanishish</button>
+      </div>
+      <img src="./youtube-embed.pmg" alt="" />
+      </div>
+ 
+      
+    </div>
+  )
+}
+
+export default App
